bb8b63ef7a3a2462b74b7923e0894aa66a1edd73
huyngqa <104546437+huyngqa@users.noreply.github.com>
2022-06-21 20:46:58 +0700

    edit

--- excer/src/ss11_dsa_stack_queue/excercise/CountCharacter.java
+++ excer/src/ss11_dsa_stack_queue/excercise/CountCharacter.java
@@ -1,9 +1,23 @@
 package ss11_dsa_stack_queue.excercise;
 
-import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CountCharacter {
     public static void main(String[] args) {
-
+        Map<Character, Integer> map = new HashMap<>();
+        String str = "nguyenminhhuy";
+        map.put(str.charAt(0), 1);
+        for (int i = 1; i < str.length(); i++) {
+            if(map.containsKey(str.charAt(i))) {
+                int count = map.get(str.charAt(i));
+                map.put(str.charAt(i), ++count);
+            } else {
+                map.put(str.charAt(i),1);
+            }
+        }
+        for (Map.Entry<Character, Integer> entrySet : map.entrySet()) {
+            System.out.println("Kí tự: " + entrySet.getKey() + " xuất hiện: " + entrySet.getValue() + " lần.");
+        }
     }
 }
