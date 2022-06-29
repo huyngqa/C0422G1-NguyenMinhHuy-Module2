bdccd66f79e4015376e625e548d5cc5b767cdf92
huyngqa <104546437+huyngqa@users.noreply.github.com>
2022-06-29 11:12:10 +0700

    bai tap

--- excer/src/ss19_string_regex/excercise/ValidatePhone.java
+++ excer/src/ss19_string_regex/excercise/ValidatePhone.java
@@ -2,8 +2,8 @@ package ss19_string_regex.excercise;
 
 public class ValidatePhone {
     public static void main(String[] args) {
-        String phone = "+84935507495";
-        String pattern = "^(\\+84|0)[0-9]{9}$";
+        String phone = "(84)-(0935507495)";
+        String pattern ="\\(84\\)-\\(0\\d{9}\\)";
         boolean check = phone.matches(pattern);
         String valid;
         if(check)
